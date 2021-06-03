fc216fea31e56614414c783efcd57cf8faf8620d
Boris Salleg <30385889+borissalleg@users.noreply.github.com>
2021-06-02 21:46:55 -0500

    Initial commit

---
+++ js/materialize.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: false,
+    duration: 200,
+    noWrap: false
+  });
+});
+
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carousel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: ".carousel__indicadores",
+    arrows: {
+      prev: ".carousel__anterior",
+      next: ".carousel__siguiente"
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 450,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      }
+    ]
+  });
+});
